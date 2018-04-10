b82823fe4bbd5a62e0b7f12d24bd84c247fc2456
palca2093 <palca2093@gmail.com>
2018-04-10 16:45:58 -0600

    add files
    Add files

---
+++ Ejemplos/Clase 4/Debug/subdir_vars.mk
@@ -0,0 +1,49 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../main.cpp 
+
+LDS_SRCS += \
+../msp432p401r.lds 
+
+C_SRCS += \
+../startup_msp432p401r_gcc.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./startup_msp432p401r_gcc.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./main.o \
+./startup_msp432p401r_gcc.o \
+./system_msp432p401r.o 
+
+CPP_DEPS += \
+./main.d 
+
+OBJS__QUOTED += \
+"main.o" \
+"startup_msp432p401r_gcc.o" \
+"system_msp432p401r.o" 
+
+C_DEPS__QUOTED += \
+"startup_msp432p401r_gcc.d" \
+"system_msp432p401r.d" 
+
+CPP_DEPS__QUOTED += \
+"main.d" 
+
+CPP_SRCS__QUOTED += \
+"../main.cpp" 
+
+C_SRCS__QUOTED += \
+"../startup_msp432p401r_gcc.c" \
+"../system_msp432p401r.c" 
+
+
