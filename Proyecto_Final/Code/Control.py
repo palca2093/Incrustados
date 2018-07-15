19593d6c5845ec7cf36a6f0a609dd0ef9421b033
Pablo <palca2093@gmail.com>
2018-07-15 17:55:11 -0600

    Test

--- Proyecto_Final/Code/Control.py
+++ Proyecto_Final/Code/Control.py
@@ -2,5 +2,13 @@ import gpiozero
 
 
 button1 = Button(5)
-button2 = 
+button2 = Button(6)
+button3 = Button(13)
+button4 = Button(19)
 
+
+while True:
+    if button1.is_pressed:
+        print("Button is pressed")
+    else:
+        print("Button is not pressed")
